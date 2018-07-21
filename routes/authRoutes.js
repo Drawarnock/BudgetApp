2252f870e28a444e88b85916ead27540eb2b86da
Konrad Wisłocki <wislockik23@gmail.com>
2018-07-21 11:54:34 +0200

    fixed scope property in linkedin strategy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,7 @@ router.get('/auth/twitter', passport.authenticate('twitter', {
 );
 
 router.get('/auth/linkedin', passport.authenticate('linkedin', {
-    scope: ['profile', 'email']
+    scope: ['r_basicprofile', 'r_emailadress']
     })
 );
 
